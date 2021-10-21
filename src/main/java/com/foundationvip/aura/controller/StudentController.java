acaa7d9eb4d2160f22e3826320c0962acd13898e
Carlos Aurélio <carlosync@gmail.com>
2021-10-20 21:27:15 -0400

    Method lits students in StudentsController

--- src/main/java/com/foundationvip/aura/controller/StudentController.java
+++ src/main/java/com/foundationvip/aura/controller/StudentController.java
@@ -1,11 +1,37 @@
 package com.foundationvip.aura.controller;
 
+import com.foundationvip.aura.model.Gender;
+import com.foundationvip.aura.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    @GetMapping
+    public List<Student> getAllStudents(){
+        List<Student> students = Arrays.asList(
+                new Student(
+                        1L,
+                        "Carlos",
+                        "2021020204",
+                        "carlos@gmai.com",
+                        Gender.MALE
+                ),
+                new Student(
+                        2L,
+                        "Jamires",
+                        "2021050721",
+                        "jamires@gmai.com",
+                        Gender.FEMALE
+                )
+        );
+        return students;
+    }
 
 }
